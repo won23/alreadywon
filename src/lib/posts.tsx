6ad07d3736f88eaad95af46035df47f0f8df4acf
Tim Won <timothy.k.won@pwc.com>
2020-09-01 19:22:11 -0400

    chore: move toplevel files to /src directory

--- src/lib/posts.tsx
+++ src/lib/posts.tsx
@@ -4,7 +4,7 @@ import matter from 'gray-matter';
 import remark from 'remark';
 import html from 'remark-html';
 
-const postsDirectory = path.join(process.cwd(), 'posts');
+const postsDirectory = path.join(process.cwd(), 'src/posts');
 export interface IPost {
   id?: string;
   title?: string;
