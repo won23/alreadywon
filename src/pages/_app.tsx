e441f74f9eb0e691248e0bad2138ad9b4538e0e8
Tim Won <timothy.k.won@pwc.com>
2020-09-13 09:40:02 -0400

    feat: add GA tracking to app

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -1,10 +1,13 @@
 import { ThemeProvider, CSSReset } from '@chakra-ui/core';
 import '../styles/global.css';
 import customTheme from '../styles/theme';
-
+import Router from 'next/router';
+import * as gtag from 'src/lib/gtag'; //https://hoangtrinhj.com/using-google-analytics-with-next-js
 interface IProps {
   [key: string]: any;
 }
+// Notice how we track pageview when route is changed
+Router.events.on('routeChangeComplete', (url) => gtag.pageView(url));
 
 export default function App({ Component, pageProps }: IProps) {
   return (
--- src/pages/_document.tsx
+++ src/pages/_document.tsx
@@ -1,4 +1,5 @@
 import Document, { Html, Head, Main, NextScript } from 'next/document';
+import { GA_TRACKING_ID } from 'src/lib/gtag';
 
 class MyDocument extends Document {
   // static async getInitialProps(ctx) {
@@ -18,6 +19,25 @@ class MyDocument extends Document {
             href="https://fonts.googleapis.com/css2?family=Alegreya+Sans&display=swap"
             rel="stylesheet"
           ></link>
+          <script
+            async
+            src={`https://www.googletagmanager.com/gtag/js?id=${GA_TRACKING_ID}`}
+          />
+          <script
+            dangerouslySetInnerHTML={{
+              __html: `
+                <script>
+                  window.dataLayer = window.dataLayer || [];
+                  function gtag(){dataLayer.push(arguments);}
+                  gtag('js', new Date());
+                
+                  gtag('config', '${GA_TRACKING_ID}', {
+                    page_path: window.location.pathname,
+                  });
+                </script>
+              `,
+            }}
+          />
         </Head>
 
         <body>
